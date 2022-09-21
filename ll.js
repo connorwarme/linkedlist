0667eaed00ea1faab231ed6764105bfde2899b71
connorwarme <connor.warme@gmail.com>
2022-09-21 15:09:01 -0600

    Finish fns, incl. extra credit challenge

--- ll.js
+++ ll.js
@@ -1,9 +1,3 @@
-// Extra Credit
-
-// insertAt(value, index) that inserts a new node with the provided value at the given index.
-// removeAt(index) that removes the node at the given index.
-
-// Extra Credit Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their nextNode link updated.
 
 const NodeFactory = (input) => {
     return {
@@ -60,8 +54,10 @@ const LinkedList = (() => {
     }
     const at = (index) => {
     // at(index) returns the node at the given index
-    if (index >= length) {
+    if (index >= length || index < 0) {
         console.log(`That index position does not exist in current list`);
+        // not sure if I should return null here or not.. !!!
+        return null;
     } else {
         let next = headP;
         for (let i = 0; i < index; i++) {
@@ -71,21 +67,86 @@ const LinkedList = (() => {
     }
     }
     const pop = () => {
-    // pop removes the last element from the list
-    // update tailP
-    // update length
-    
+        removeAt(length-1);
     }
     const contains = (value) => {
     // contains(value) returns true if the passed in value is in the list and otherwise returns false.
+        let current = headP;
+        while (current != null && current.value != value) {
+            current = current.nextNode;
+        }
+        if (current != null) {
+            return current;
+        } else {
+            return false;
+        }
     }
     const find = (value) => {
     // find(value) returns the index of the node containing value, or null if not found.
+        let index = 0;
+        let current = headP;
+        while (current != null && current.value != value) {
+            current = current.nextNode;
+            index++;
+        }
+        if (current != null) {
+            return index;
+        } else {
+            return null;
+        }
     }
     const toString = () => {
     // toString represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+        let current = headP;
+        let text = "Values: ";
+        for (let i = 0; i < length; i++) {
+            text += `${current.value} -> `;
+            current = current.nextNode;
+        }
+        text += `null.`
+        return text;
     }
-    return { append, prepend, size, head, tail, at }
+    const insertAt = (value, index) => {
+        let current = at(index);
+        if (index === 0) {
+            prepend(value);
+        } else if (index === length -1) {
+            append(value);
+        } else {
+            let previous = at(index - 1);
+            let newNew = NodeFactory(value);
+            newNew.nextNode = current;
+            previous.nextNode = newNew;
+            length++;
+        }
+    }
+    const removeAt = (index) => {
+        let current = at(index);
+        if (index === 0) {
+            headP = current.nextNode;
+        } else {
+            let previous = at(index - 1);
+            previous.nextNode = current.nextNode;
+            if (current === tailP) {
+                tailP = previous;
+            }
+        }
+        length--;
+        console.log(tailP);
+    }
+    return { 
+        append, 
+        prepend, 
+        size, 
+        head, 
+        tail, 
+        at, 
+        pop, 
+        contains, 
+        find, 
+        toString,
+        insertAt,
+        removeAt }
 })(); 
 
 LinkedList.append(5);
